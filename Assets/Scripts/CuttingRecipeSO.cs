712ad232132d5a0d5adc1d21031182e0173219e2
Matodor <matodor-zlo@yandex.ru>
2023-09-19 00:36:37 +0400

    feat: added CuttingRecipeSO

---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CuttingRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO Input => _input;
+    public KitchenObjectSO Output => _output;
+
+    [SerializeField]
+    private KitchenObjectSO _input;
+
+    [SerializeField]
+    private KitchenObjectSO _output;
+}
